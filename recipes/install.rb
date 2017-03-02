003a7a89a71900b0aa5f60b04fe34fbdfcbdf97c
Luke Watson <luwatson90@gmail.com>
2017-03-01 22:49:04 -0500

    rework logic for installation

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,19 +2,19 @@ executable_name = 'IBSetupConsole.exe'
 file_name = File.join(Chef::Config[:file_cache_path], executable_name)
 
 remote_file file_name do
+  only_if { node['incredibuild'].attribute?['url'] }
   source source_url
   action :create
 end
 
-if File.exist?(file_name)
-  command = [file_name, '/install"']
-  if node['incredibuild']['install']['installdir']
-    command << "/INSTALLDIR=#{node['incredibuild']['install']['installdir']}"
-  end
-  command << "/COORDINATOR=#{node['incredibuild']['install']['coordinator']}"
-  execute 'Install Incredibuild' do
-    command = command.join(' ')
-    cwd Chef::Config[:file_cache_path]
-    live_stream true
-  end
+command = [file_name, '/install"', "/COORDINATOR=#{node['incredibuild']['install']['coordinator']}"]
+if node['incredibuild']['install']['installdir']
+  command << "/INSTALLDIR=#{node['incredibuild']['install']['installdir']}"
+end
+
+execute 'Install Incredibuild' do
+  only_if { File.exist?(file_name) }
+  command command.join(' ')
+  cwd Chef::Config[:file_cache_path]
+  live_stream true
 end
